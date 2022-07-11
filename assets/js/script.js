483fd14455449668633308b5ba371a8dc4ed2f2e
sadafsafi22 <safisadaf96@gmail.com>
2022-07-11 12:55:07 +0000

    Add new input for date and allow user to enter the date as well

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // declare varabliles to get the value of input
-var inputElement  = document.querySelector('#new-todo input');
-// declare an array to save todo's after adding 
+var inputElement  = document.getElementById('todoText');
+var InputDate = document.getElementById('todoDate');
+// declare an array to save todos
 let todoArray = [{'text':'','date':'',index:'0'}];
 
 // handeling the add button and check if the input is Entering Something
@@ -9,49 +10,106 @@ document.getElementById('add').addEventListener('click',function(){
         
         //change the border color while nthing is enter and click on add button
         inputElement.style.borderColor = "red";
-        alert("Please Enter what to do");
+        alert("Please Enter what todo");
         inputElement.focus();
+
+    }else if(InputDate.value === ''){
+
+        InputDate.style.borderColor = "red";
+        alert("Please Select a Date for todo");
+        InputDate.focus();
 
     }else{
         
-        listTodos();
+        //alert(InputDate.value);
+        //listTodos();
+        createTodo();
     }
 });
 
-// handel the Enter key on keybord while press the Enter button
+// handel the Enter key on keybord while press the Enter button for input type text
 inputElement.addEventListener('keydown',function(event){
     if(event.key === "Enter"){
-        if(inputElement.value.length != 0){
-            listTodos();
+        if(inputElement.value.length === 0){
+        
+            //change the border color while nthing is enter and click on add button
+            inputElement.style.borderColor = "red";
+            alert("Please Enter what todo");
+            inputElement.focus();
+    
+        }else if(InputDate.value === ''){
+    
+            InputDate.style.borderColor = "red";
+            alert("Please Select a Date for todo");
+            InputDate.focus();
+    
+        } else {
+            createTodo()
+        }
+    }
+});
+
+
+// handel the Enter key on keybord while press the Enter button for input type date
+InputDate.addEventListener('keydown',function(event){
+    if(event.key === "Enter"){
+        if(inputElement.value.length === 0){
+        
+            //change the border color while nthing is enter and click on add button
+            inputElement.style.borderColor = "red";
+            alert("Please Enter what todo");
+            inputElement.focus();
+    
+        }else if(InputDate.value === ''){
+    
+            InputDate.style.borderColor = "red";
+            alert("Please Select a Date for todo");
+            InputDate.focus();
+    
+        } else {
+            createTodo()
         }
     }
 });
 
 /**
+ * createTodo function this function is for adding new todo at todoArray object
  * 
- * listTodos  function first push our new todo on todoArray and  list all our todo's which 
- * we have add to our list by Enter the text and press the Add button or Entering the Enter button
+ */
+function createTodo(){
+    var todoDate=new Date(InputDate.value);
+    let arrayIndexGenerate = (inputElement.value.length != 0 ) ? inputElement.value.length + Math.round((Math.random() * 100)) : 0;
+    todoArray.push({"text":inputElement.value,"date":todoDate ,index:arrayIndexGenerate});
+    
+    inputElement.value = "";
+    InputDate.value = "";
+    InputDate.style.borderColor = '#d1d3d4';
+
+    listTodos();
+}
+
+/**
+ * 
+ * listTodos function is for listing todos and after adding 
+ * and deleting new todo, it refresh the list 
  * 
  */
 function listTodos(){
-    let dateTime = new Date().toLocaleDateString() +'  '+ new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
-    let arrayIndexGenerate = (inputElement.value.length != 0 ) ? inputElement.value.length + Math.round((Math.random() * 100)) : 0;
-    todoArray.push({"text":inputElement.value,"date":dateTime,index:arrayIndexGenerate});
+
     let p ='<ul>';
     for(let todo of todoArray){
         if(todo.index != 0){
-            p += `<li><button onclick="deleteFunction('${todo.index}')" class="btnDelete">Delete</button><input type="checkbox" onclick="changeWhileChecked(event)"><span class="todoText"></span>${todo.text}</span><span id="dateSpan">${todo.date}</span></li>`;
+            p += `<li><button onclick="deleteFunction('${todo.index}')" class="btnDelete">Delete</button><input type="checkbox" onclick="changeWhileChecked(event)"><span class="todoText"></span>${todo.text}</span><span id="dateSpan">${todo.date.toLocaleDateString("en-US")}</span></li>`;
         }
     }
     p +='</ul>';
     document.getElementById('todo-list').innerHTML = p;
-    inputElement.value = "";
+    
 }
 
 //change the border color from red  while typing on the input
 inputElement.addEventListener('keypress',function(){
     if(inputElement.value.length != 0){
-
         inputElement.style.borderColor = "#90b4d8";
     }
 });
@@ -63,7 +121,7 @@ function deleteFunction(liContent=""){
     listTodos();
 }
 
-//
+// change the textDecoration to line-throuth when checkbox is checked 
 function changeWhileChecked(event){
     const a = event.target.closest('input').checked;
     if(a)
